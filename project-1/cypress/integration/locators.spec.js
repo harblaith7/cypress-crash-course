d38604220a79e44890dbefd5d5e873abf77825e3
Dawlatzai <dawlatzai.ghousi@gmail.com>
2022-07-26 12:27:04 +0430

    Retrieve some html elements by cypress built in methods

--- project-1/cypress/integration/locators.spec.js
+++ project-1/cypress/integration/locators.spec.js
@@ -2,9 +2,31 @@
 
 describe('Locators', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/elements')
   })
-  it('See the locator page', () => {
-    expect(true).to.equal(true)
+  it('locating elements with get', () => {
+    // get all elements by tag name
+    cy.get('button')
+
+    // get elements by its className
+    cy.get('.btn-with-class')
+
+    // get elements with attributes
+    cy.get("[class='Elements-btn btn-with-class']")
+    cy.get("[class='Elements-btn btn-with-class more-btn-classes']")
+
+    // get elements by id
+    cy.get("[id='btn-with-id']")
+    cy.get('#btn-with-id')
+
+    // get elements by type name
+    cy.get("[type='submit']")
+
+    cy.get('button.Elements-btn')
+
+    // get elements with specific data test id
+    cy.get("[data-cy='btn-id-1']")
+
+    cy.getByTestId('btn-id-1')
   })
 })
